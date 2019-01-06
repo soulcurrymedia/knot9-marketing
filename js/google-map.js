b99ea606fd3bf32afa779e0ab793d07b310c0730
Mihir Kumar Thakur <mihirthakur.cse@gmail.com>
2019-01-06 22:16:14 +0530

    working forms

--- js/google-map.js
+++ js/google-map.js
@@ -1,4 +1,6 @@
-var position = { lat: 40.730610 , lng: -73.935242 }	
+
+
+var position = { lat: 28.3979828 , lng: 77.3107919 }	
 
 // Map style
 var style = [
@@ -213,4 +215,31 @@ var map = new google.maps.Map( document.getElementById('contact-map') , options)
 var marker = new google.maps.Marker({
     position: position,
     map: map,
+
 });
+
+
+// hack Google Maps to bypass API v3 key (needed since 22 June 2016 http://googlegeodevelopers.blogspot.com.es/2016/06/building-for-scale-updates-to-google.html)
+var target = document.head;
+var observer = new MutationObserver(function(mutations) {
+    for (var i = 0; mutations[i]; ++i) { // notify when script to hack is added in HTML head
+        if (mutations[i].addedNodes[0].nodeName == "SCRIPT" && mutations[i].addedNodes[0].src.match(/\/AuthenticationService.Authenticate?/g)) {
+            var str = mutations[i].addedNodes[0].src.match(/[?&]callback=.*[&$]/g);
+            if (str) {
+                if (str[0][str[0].length - 1] == '&') {
+                    str = str[0].substring(10, str[0].length - 1);
+                } else {
+                    str = str[0].substring(10);
+                }
+                var split = str.split(".");
+                var object = split[0];
+                var method = split[1];
+                window[object][method] = null; // remove censorship message function _xdc_._jmzdv6 (AJAX callback name "_jmzdv6" differs depending on URL)
+                //window[object] = {}; // when we removed the complete object _xdc_, Google Maps tiles did not load when we moved the map with the mouse (no problem with OpenStreetMap)
+            }
+            observer.disconnect();
+        }
+    }
+});
+var config = { attributes: true, childList: true, characterData: true }
+observer.observe(target, config);
